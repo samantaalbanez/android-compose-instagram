2db81313aa9c9310dfeb10f825d21f9f95d02705
Samanta Albanez <samanta.albanez@mercadolivre.com>
2023-04-02 11:00:40 -0300

    [Feature] Add viewModel

--- app/src/main/java/com/android/compose_instagram/MainActivity.kt
+++ app/src/main/java/com/android/compose_instagram/MainActivity.kt
@@ -6,13 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.android.compose_instagram.login.ui.LoginScreen
 import com.android.compose_instagram.login.ui.LoginViewModel
 import com.android.compose_instagram.ui.theme.ComposeinstagramTheme
-import com.android.compose_instagram.login.ui.LoginScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,18 +22,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeinstagramTheme {
-        Greeting("Android")
     }
 }
